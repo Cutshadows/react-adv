b9d874d3d3292db4afc69df43662a8b0bc219b3c
douglas_barraza <douglas.barraza@externos-cl.cencosud.com>
2022-04-22 00:02:59 -0400

    update section

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,56 +1,45 @@
+import { createContext, Fragment} from 'react';
 import styles from '../styles/styles.module.css';
-import imgPython from '../assets/scope_python.webp';
 import { useProduct } from '../hooks/useProduct';
-
-interface Props {
-	product: Product;
-}
-
-interface Product {
-	id: string;
-	title: string;
-	img?: string;
-}
-
-export const ProductImage = ({ img = '' }) : JSX.Element => {
-	return (
-		<img src={img ? img : imgPython} alt={img} className={styles.productImg} />
-	);
-};
-
-export const ProductTitle = ({ title }: { title: string }) =><span className={styles.productDescription}>{title}</span>;
+import { ProductCardProps } from '../interface/Product.dto';
 
 
-interface ProductButtonsProps {
-	counter: number;
-	increaseBy: (value: number) => void;
-}
 
-export const ProductButtons = ({
-	counter,
-	increaseBy,
-}: ProductButtonsProps) => {
-	return (
-		<div className={styles.buttonsContainer}>
-			<button className={styles.buttonMinus} onClick={() => increaseBy(-1)}>
-				-
-			</button>
-			<div className={styles.countLabel}> {counter} </div>
-			<button className={styles.buttonAdd} onClick={() => increaseBy(1)}>
-				+
-			</button>
-		</div>
-	);
-};
+export const ProductContext = createContext({ ProductState: { counter: 0, product: { id: '', title: '', img: 'https://w7.pngwing.com/pngs/364/982/png-transparent-python-programming-language-computer-programming-language-angle-text-computer-thumbnail.png' }, increaseBy: (value: number) => {} } });
+const { Provider } = ProductContext;
 
-export const ProductCard = ({ product }: Props) => {
+
+
+
+
+
+
+
+
+
+export const ProductCard = ({ children, product, className, style }: ProductCardProps) => {
+
 	const { counter, increaseBy } = useProduct();
 
+
 	return (
-		<div className={styles.productCard}>
-			<ProductImage img={product.img} />
-			<ProductTitle title={product.title} />
-			<ProductButtons counter={counter} increaseBy={increaseBy} />
-		</div>
+		<Fragment>
+			<Provider value={{
+				ProductState:{
+					counter,
+					increaseBy,
+					product,
+				}
+			}}>
+				<div className={`${styles.productCard } ${className}`} style={style}>
+					{children}
+					{/* <ProductImage img={product.img} />
+					<ProductTitle title={product.title} />
+					<ProductButtons counter={counter} increaseBy={increaseBy} /> */}
+				</div>
+			</Provider>
+		</Fragment>
 	);
 };
+
+
--- src/02-component-patterns/pages/ShoppingPages.tsx
+++ src/02-component-patterns/pages/ShoppingPages.tsx
@@ -1,4 +1,6 @@
-import { ProductCard } from '../components/ProductCard';
+import { ProductButtons, ProductImage, ProductTitle} from '../components';
+import {ProductCard} from '../components';
+import '../styles/components/pages/shopping.component.css';
 
 const ShoppingPages = () => {
 	const product = {
@@ -8,15 +10,41 @@ const ShoppingPages = () => {
 	};
 	
 	return (
-		<div>
+		<div  >
 			<h1>Shopping Store</h1>
 			<hr />
 			<div style={{
 				display: 'flex',
 				flexDirection: 'row',
 				flexWrap: 'wrap'
-			}}>
-				<ProductCard product={product} />
+			}} >
+				<ProductCard product={product}  className="bg-dark text-white">
+					<ProductCard.Image img='' className="custom-image" />
+					<ProductCard.Title title={'Café'} className="text-white text-bold"/>
+					<ProductCard.Buttons className="text-white custom-buttons"/>
+					{/* <ProductCard.Buttons increaseBy={function (value:number):void {
+					throw new Error('Function not implemented.');
+					}}
+					counter={0} /> */}
+				</ProductCard>
+				
+				<ProductCard product={product} className="bg-dark text-white" >
+					<ProductImage img=''  className="custom-image"/>
+					<ProductTitle title={'Café'} className="text-white text-bold" />
+					<ProductButtons className="text-white custom-buttons"/>
+				</ProductCard>
+				<ProductCard product={product} style={{
+					backgroundColor: '#70D1F8',
+				}}>
+					<ProductImage img='' style={{boxShadow: '10px 10px 10px rgba(0, 0, 0, 0.5)'}}  />
+					<ProductTitle title={'Café'} style={{
+						fontWeight: 'bold',
+					}} />
+					<ProductButtons  style={{
+						display: 'flex',
+						justifyContent: 'end',
+					}}/>
+				</ProductCard>
 			</div>
 		</div>
 	);
