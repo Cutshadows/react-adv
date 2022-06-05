16784873b3cff5c44594ee7a8029fda19c976be4
douglas_barraza <douglas.barraza@externos-cl.cencosud.com>
2022-06-05 14:00:39 -0400

    uppdate course fherrera

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -5,7 +5,6 @@ import { Routes, Route, NavLink, Navigate } from 'react-router-dom';
 import logo from '../logo.svg';
 import { routes } from './routes';
 
-// import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
 
 export const Navigation = () => {
 	return (
@@ -36,9 +35,6 @@ export const Navigation = () => {
 							)
 							)
 						};
-						{/* <Route path="lazy2" element={ <LazyPage2 /> } /> */}
-						{/* <Route path="lazy3" element={ <LazyPage3 /> } /> */}
-
 						<Route path="/*" element={ <Navigate to={routes[0].to } replace /> } />
 					</Routes>
 				</div>
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,5 @@
 import { lazy, LazyExoticComponent } from 'react';
-import { NoLazy } from '../01-lazyload/pages/NoLazy';
+// import { NoLazy } from '../01-lazyload/pages/NoLazy';
 // import { LazyPage1 } from '../01-lazyload/pages';
 // import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
 
@@ -12,20 +12,9 @@ interface Route {
     Component: JSXComponent | LazyExoticComponent<JSXComponent>
 }
 
-// const LazyPage1= lazy(()=>import(/* webpackChunkName: "LazyPage1" */ '../01-lazyload/pages/lazyPage1'));
-// const LazyPage2= lazy(()=>import(/* webpackChunkName: "LazyPage2" */ '../01-lazyload/pages/lazyPage2'));
-// const LazyPage3= lazy(()=>import(/* webpackChunkName: "LazyPage3" */ '../01-lazyload/pages/lazyPage3'));
-// const LazyLayout= lazy(()=>import(/* webpackChunkName: "LazyLayout"*/ '../01-lazyload/layout/LazyLayout'));
 
 const ShoppingPages= lazy(()=>import(/* webpackChunkName: "ShoppingStore"*/ '../02-component-state-initializer/pages/ShoppingPages'));
 
-// lint-disable-next-line
-// {
-//     to: 'lazy2',
-//     path: 'lazy2',
-//     Component: LazyPage2,
-//     name: 'lazy-2'
-// },
 
 export const routes: Route[] = [
 	{	
@@ -34,10 +23,4 @@ export const routes: Route[] = [
 		Component: ShoppingPages,
 		name: 'shopping'
 	},
-	{
-		to: '/no-lazy',
-		path: 'no-lazy',
-		Component: NoLazy,
-		name: 'No Lazy'
-	}
 ];
